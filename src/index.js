a2da2e750fb2b670a1366757c9b177d575fd058a
Pamanix <pahrodr@gmail.com>
2024-05-28 14:01:27 -0300

    ajustes tela de encontro

--- src/index.js
+++ src/index.js
@@ -114,3 +114,26 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth', // exibe o mês como padrão
+    // adicione seus eventos aqui, se necessário
+    events: [
+      {
+        title: 'Evento 1',
+        start: '2024-05-01'
+      },
+      {
+        title: 'Evento 2',
+        start: '2024-05-15',
+        end: '2024-05-17'
+      }
+      // você pode adicionar mais eventos conforme necessário
+    ]
+  });
+
+  calendar.render();
+});
